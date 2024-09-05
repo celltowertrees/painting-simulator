ddf885accd2d5ed360a8ca6db98cfb9124305ec4
Kelly M <kelly.nicole.monson@gmail.com>
2024-09-05 22:37:04 +0100

    grid

--- src/lib/components/sphere.svelte
+++ src/lib/components/sphere.svelte
@@ -1,14 +1,12 @@
 <script>
-    import { getLight } from '../../lib/utils/mixers.js';
+    import { getLight, getShadow } from '../../lib/utils/mixers.js';
 
     export let light;
     export let base;
-    export let shadow;
 
     $: palette = {
         base: base,
-        shadow: shadow,
-        // shadow: chroma.mix(initial.groundBase, getShadow(initial.sphereBase), 0.85, 'lab'), // account for reflection of ground here IF bg is brighter than object
+        shadow: getShadow(base),
         light: getLight(light, base)
     }
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,37 @@
 <script>
-  import chroma from 'chroma-js';
-  import { getLight, getShadow } from '../lib/utils/mixers.js';
-  import Sphere from '../lib/components/sphere.svelte';
+    import chroma from "chroma-js";
+    import { getShadow } from "../lib/utils/mixers.js";
+    import Sphere from "../lib/components/sphere.svelte";
 
-  let initial = {
-    sunlight: '#ffdb00',
-    sphereBase: '#00ac73',
-    skyBase: '#c3e9ff',
-    groundBase: '#671f1f',
-  }
+    let initial = {
+        sunlight: "#ffdb00",
+        sphereBase: "#00ac73",
+        skyBase: "#c3e9ff",
+        groundBase: "#671f1f",
+    };
 
-  $: skyBase = chroma.mix(initial.sunlight, initial.skyBase);
+    $: skyBase = initial.skyBase;
 
-  $: shadows = {
-    sphere: chroma.mix(initial.groundBase, getShadow(initial.sphereBase), 0.85, 'lab'), // account for reflection of ground here IF bg is brighter than object
-    ground: chroma.mix(initial.sphereBase, getShadow(initial.groundBase), 0.85, 'lab'), // account for reflection of sphere here
-  }
-
-  $: light = {
-    ground: getLight(initial.sunlight, initial.groundBase),
-  }
-
-  $: palette = [
-    initial.sunlight,
-    skyBase,
-    initial.sphereBase,
-    shadows.sphere,
-    light.ground,
-    initial.groundBase,
-    shadows.ground,
-    initial.background,
-  ];
-
+    $: shadows = {
+        ground: chroma.mix(
+            initial.sphereBase,
+            getShadow(initial.groundBase),
+            0.85,
+            "lab",
+        ), // account for reflection of sphere here
+    };
 </script>
 
 <svelte:head>
-  <title>PAINTING SIMULATOR</title>
+    <title>PAINTING SIMULATOR</title>
 </svelte:head>
 
-<div class="screen">
-    <div class="background" style="--background: {skyBase};">
-      <div class="item-1">
-        <Sphere
-            light={initial.sunlight}
-            shadow={shadows.sphere}
-            base={initial.sphereBase}
-        />
-      </div>
-
-      <div class="ground" style="
-        --light: {initial.sunlight};
-        --ground: {initial.groundBase};
-        --dark: {shadows.ground};
-      ">
-      </div>
+<div class="screen" style="--background: {skyBase}; --ground: {initial.groundBase};">
+    <div class="item-1">
+        <Sphere light={initial.sunlight} base={initial.sphereBase} />
     </div>
-    <div class="palette">
-        {#each palette as color}
-          <div class="color" style="background: {color};"></div>
-        {/each}
+    <div class="item-2">
+        <Sphere light={initial.sunlight} base={initial.sphereBase} />
     </div>
 </div>
 
@@ -70,60 +43,32 @@
     </div>
     <div class="sunlight-controls">
         <!-- <input type="range" min="0" max="100" bind:value={initial.sunlight} /> -->
-         <input type="color" bind:value={initial.sunlight} />
+        <input type="color" bind:value={initial.sunlight} />
     </div>
 </div>
 
 <style>
-.screen {
-    display: flex;
-    gap: 0.5rem;
-}
+    .screen {
+        height: 70vh;
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 0.5rem;
+        overflow: hidden;
+        position: relative;
+        background: linear-gradient(
+            180deg,
+            var(--background) 0%,
+            var(--background) 48%,
+            var(--ground) 55%,
+            var(--ground) 100%
+        );
+    }
 
-.background {
-  background-color: var(--background);
-  position: relative;
-  height: 70vh;
-  width: 100%;
-  z-index: -2;
-  overflow: hidden;
-}
-
-.item-1 {
-    position: absolute;
-    top: 12vh;
-    left: 40vw;
-    z-index: 2;
-}
-
-.ground {
-  background: linear-gradient(
-    -230deg,
-    var(--light) 0%,
-    var(--ground) 75%,
-    var(--ground) 95%,
-    var(--dark) 100%
-  );
-  /* background-color: var(--ground); */
-  position: absolute;
-  width: 100%;
-  bottom: 0;
-  height: 45vh;
-  z-index: -1;
-}
-
-.control-panel {
-
-}
-
-.palette {
-  display: flex;
-  flex-direction: column;
-}
-
-.palette .color {
-  height: 50px;
-  width: 50px;
-}
-
+    .item-1, .item-2 {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
