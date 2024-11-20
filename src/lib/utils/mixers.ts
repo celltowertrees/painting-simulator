ea58db3c33af01710461126e412332e19a2ccef6
Kelly Monson <kellymonson@signalnoise.io>
2024-11-20 22:17:57 +0000

    add in landscape svg

--- src/lib/utils/mixers.ts
+++ src/lib/utils/mixers.ts
@@ -1,13 +1,13 @@
 import chroma from 'chroma-js';
 
 export function getLight(light: string, baseColor: string) {
-  return chroma.mix(light, baseColor, 0.9, 'lab').brighten();
+  return chroma.mix(light, baseColor, 0.7, 'lab').brighten(0.5);
 }
 
 export function getShadow(light: string, baseColor: string) {
   const complement = chroma(light).set('hsl.h', '+180'); 
 
-  return chroma.mix(complement, baseColor, 0.5, 'lab').darken();
+  return chroma.mix(complement, baseColor, 0.6, 'lab').darken();
 
   // return chroma(baseColor).darken();
 }
